7a4d4a9ffbbc9802616f0eeff0522eeade1ddb05
Carlos Ramirez <crramirez@gmail.com>
2021-05-30 19:36:09 +0200

    Only if great than 0

--- linux_files/upgrade.sh
+++ linux_files/upgrade.sh
@@ -72,7 +72,7 @@ fi
 if [[ -z ${WSL2} ]]; then
   gpgcheck_enabled=$(sudo dnf config-manager --dump '*' | grep -c "gpgcheck = 1")
 
-  if [[ ${gpgcheck_enabled} -ge 0 ]]; then
+  if [[ ${gpgcheck_enabled} -gt 0 ]]; then
     sudo curl -L -f "${BASE_URL}/linux_files/check-dnf.sh" -o /etc/profile.d/check-dnf.sh
     sudo curl -L -f "${BASE_URL}/linux_files/check-dnf" -o /usr/bin/check-dnf
     echo '%wheel   ALL=NOPASSWD: /usr/bin/check-dnf' | sudo EDITOR='tee -a' visudo --quiet --file=/etc/sudoers.d/check-dnf
