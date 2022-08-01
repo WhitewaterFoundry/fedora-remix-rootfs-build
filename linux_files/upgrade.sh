6d5fe16ed5577e45a140219ae4e8d43f92862b86
Carlos Ramirez <crramirez@gmail.com>
2022-08-01 22:17:28 +0200

    Create a directory during the update

--- linux_files/upgrade.sh
+++ linux_files/upgrade.sh
@@ -86,6 +86,7 @@ sudo curl -L -f "${base_url}/linux_files/start-systemd.sudoers" -o /etc/sudoers.
 sudo curl -L -f "${base_url}/linux_files/start-systemd.sh" -o /usr/local/bin/start-systemd
 sudo curl -L -f "${base_url}/linux_files/wsl2-xwayland.service" -o /etc/systemd/system/wsl2-xwayland.service
 sudo curl -L -f "${base_url}/linux_files/wsl2-xwayland.socket" -o /etc/systemd/system/wsl2-xwayland.socket
+sudo mkdir -p /etc/systemd/system/sockets.target.wants
 sudo ln -sf ../wsl2-xwayland.socket /etc/systemd/system/sockets.target.wants/
 
 
