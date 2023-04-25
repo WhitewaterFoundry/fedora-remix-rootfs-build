c24e11db884127ebfc8a1a9668e05416aa22c011
Carlos Ramirez <crramirez@gmail.com>
2023-04-26 00:13:40 +0200

    Fix mesa in the image creation

--- create-targz.sh
+++ create-targz.sh
@@ -135,8 +135,8 @@ EOF
   echo "##[section] 'Install MESA"
   systemd-nspawn -q --resolv-conf="replace-host" -D "${TMPDIR}"/dist --pipe /bin/bash <<EOF
 dnf -y install 'dnf-command(versionlock)'
-dnf -y install --allowerasing --nogpgcheck mesa-dri-drivers-23.0.2-wsl_2 mesa-libGL-23.0.2-wsl_2 mesa-va-drivers-23.0.2-wsl_2 mesa-vdpau-drivers-23.0.2-wsl_2 glx-utils vdpauinfo libva-utils
-dnf versionlock add mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi mesa-va-drivers mesa-vdpau-drivers
+dnf -y install --allowerasing --nogpgcheck mesa-dri-drivers-23.0.2-wsl_3 mesa-libGL-23.0.2-wsl_3 mesa-va-drivers-23.0.2-wsl_3 mesa-vdpau-drivers-23.0.2-wsl_3 mesa-libEGL-23.0.2-wsl_3 mesa-libgbm-23.0.2-wsl_3 mesa-libxatracker-23.0.2-wsl_3 mesa-vulkan-drivers-23.0.2-wsl_3 glx-utils vdpauinfo libva-utils
+dnf versionlock add mesa-dri-drivers mesa-libGL mesa-filesystem mesa-libglapi mesa-va-drivers mesa-vdpau-drivers mesa-libEGL mesa-libgbm mesa-libxatracker mesa-vulkan-drivers
 
 /usr/sbin/groupadd -g 44 wsl-video
 
