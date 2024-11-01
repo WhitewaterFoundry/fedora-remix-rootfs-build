07de8ae52ef0678458418ce018f06fb2cbc6130c
Carlos Ramirez <crramirez@gmail.com>
2024-11-01 19:28:54 +0100

    Update mesa version identifiers in upgrade and create scripts
    Updated the identifier for the mesa version '24.2.5-1_wsl.fc41' to '24.2.5-1_wsl_2.fc41' in both upgrade.sh and create-targz.sh scripts. This ensures consistency and correctness in the versioning.

--- linux_files/upgrade.sh
+++ linux_files/upgrade.sh
@@ -54,7 +54,7 @@ sudo chmod +x /usr/local/bin/install-desktop.sh
 # Install mesa
 source /etc/os-release
 
-declare -a mesa_version=('23.1.9-wsl' '24.1.2-7_wsl.fc40' '24.2.5-1_wsl.fc41')
+declare -a mesa_version=('23.1.9-wsl' '24.1.2-7_wsl.fc40' '24.2.5-1_wsl_2.fc41')
 declare -a target_version=('39' '40' '41')
 declare -i length=${#mesa_version[@]}
 
