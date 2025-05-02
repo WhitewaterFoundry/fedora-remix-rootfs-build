14d83fc523b209ed336b37426a657ca52f8d63af
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2025-05-02 10:11:23 -0400

    without any service

--- create-targz.sh
+++ create-targz.sh
@@ -83,9 +83,9 @@ function build() {
   cp "${origin_dir}"/linux_files/fedoraremix.ico "${TMPDIR}"/dist/usr/lib/wsl/fedoraremix.ico
   cp "${origin_dir}"/linux_files/terminal-profile.json "${TMPDIR}"/dist/usr/lib/wsl/terminal-profile.json
 
-  cp "${origin_dir}"/linux_files/wsl2-xwayland.service "${TMPDIR}"/dist/etc/systemd/system/wsl2-xwayland.service
-  cp "${origin_dir}"/linux_files/wsl2-xwayland.socket "${TMPDIR}"/dist/etc/systemd/system/wsl2-xwayland.socket
-  ln -sf ../wsl2-xwayland.socket "${TMPDIR}"/dist/etc/systemd/system/sockets.target.wants/
+  #cp "${origin_dir}"/linux_files/wsl2-xwayland.service "${TMPDIR}"/dist/etc/systemd/system/wsl2-xwayland.service
+  #cp "${origin_dir}"/linux_files/wsl2-xwayland.socket "${TMPDIR}"/dist/etc/systemd/system/wsl2-xwayland.socket
+  #ln -sf ../wsl2-xwayland.socket "${TMPDIR}"/dist/etc/systemd/system/sockets.target.wants/
 
   #cp "${origin_dir}"/linux_files/wsl-links.conf "${TMPDIR}"/dist/usr/lib/tmpfiles.d/
   #mkdir -p "${TMPDIR}"/dist/usr/share/user-tmpfiles.d
