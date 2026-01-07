0098ed90b7a8f1f076f18327fc4efb8595d0a48e
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2026-01-08 00:59:58 +0100

    Apply suggestion from @crramirez

--- update-targz.sh
+++ update-targz.sh
@@ -46,7 +46,6 @@ sudo cp /etc/resolv.conf rootfs/etc/
 echo 'Setup WSLU'
 # Remove old COPR wslu repositories for all Fedora versions
 sudo rm -f rootfs/etc/yum.repos.d/wslutilties.repo
-copr_found=false
 for copr_file in rootfs/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:wslutilities:wslu*.repo; do
   if [[ -f "${copr_file}" ]]; then
     sudo rm -f "${copr_file}"
