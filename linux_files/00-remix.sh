084008d477e7ce2caeedc81bc90ecfb81fc4b171
Carlos Ramirez <crramirez@gmail.com>
2026-02-19 19:54:50 +0100

    Improve environment setup and define XDG base directories for better compatibility with WSL and SystemD.

--- linux_files/00-remix.sh
+++ linux_files/00-remix.sh
@@ -3,18 +3,51 @@
 
 save_environment() {
   {
-    echo "PATH='$PATH'"
-    echo "WSL_DISTRO_NAME='$WSL_DISTRO_NAME'"
-    echo "WSL_INTEROP='$WSL_INTEROP'"
-    echo "WSL_SYSTEMD_EXECUTION_ARGS='$WSL_SYSTEMD_EXECUTION_ARGS'"
-    echo "PULSE_SERVER='$PULSE_SERVER'"
-    echo "WAYLAND_DISPLAY='$WAYLAND_DISPLAY'"
+    [ -n "${PATH}" ] && echo "PATH='$PATH'"
+    [ -n "${WSL_DISTRO_NAME}" ] && echo "WSL_DISTRO_NAME='$WSL_DISTRO_NAME'"
+    [ -n "${WSL_INTEROP}" ] && echo "WSL_INTEROP='$WSL_INTEROP'"
+    [ -n "${WSL_SYSTEMD_EXECUTION_ARGS}" ] && echo "WSL_SYSTEMD_EXECUTION_ARGS='$WSL_SYSTEMD_EXECUTION_ARGS'"
+    [ -n "${PULSE_SERVER}" ] && echo "PULSE_SERVER='$PULSE_SERVER'"
+    [ -n "${WAYLAND_DISPLAY}" ] && echo "WAYLAND_DISPLAY='$WAYLAND_DISPLAY'"
   } >"${systemd_saved_environment}"
 }
 
 setup_interop() {
   # shellcheck disable=SC2155,SC2012
   export WSL_INTEROP="$(ls -U /run/WSL/*_interop | tail -1)"
+}
+
+define_xdg_environment() {
+  # XDG Base Directory Specification
+  # https://specifications.freedesktop.org/basedir/latest/
+
+  if [ -z "${XDG_DATA_HOME}" ]; then
+    export XDG_DATA_HOME="${HOME}/.local/share"
+  fi
+  mkdir -p "${XDG_DATA_HOME}"
+
+  if [ -z "${XDG_CONFIG_HOME}" ]; then
+    export XDG_CONFIG_HOME="${HOME}/.config"
+  fi
+  mkdir -p "${XDG_CONFIG_HOME}"
+
+  if [ -z "${XDG_STATE_HOME}" ]; then
+    export XDG_STATE_HOME="${HOME}/.local/state"
+  fi
+  mkdir -p "${XDG_STATE_HOME}"
+
+  if [ -z "${XDG_CACHE_HOME}" ]; then
+    export XDG_CACHE_HOME="${HOME}/.cache"
+  fi
+  mkdir -p "${XDG_CACHE_HOME}"
+
+  if [ -z "${XDG_DATA_DIRS}" ]; then
+    export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+  fi
+
+  if [ -z "${XDG_CONFIG_DIRS}" ]; then
+    export XDG_CONFIG_DIRS="/etc/xdg"
+  fi
 }
 
 setup_display() {
@@ -50,42 +83,40 @@ setup_display() {
     export WSL2=1
 
     if [ -n "${DISPLAY}" ]; then
+      uid="$(id -u)"
 
-        uid="$(id -u)"
+      user_path="/run/user/${uid}"
+      if [ ! -d "${user_path}" ]; then
+        sudo /usr/local/bin/create_userpath "${uid}" 2>/dev/null
+      fi
 
-        user_path="/run/user/${uid}"
-        if [ ! -d "${user_path}" ]; then
-          sudo /usr/local/bin/create_userpath "${uid}" 2>/dev/null
-        fi
+      if [ -z "$SYSTEMD_PID" ]; then
+        export XDG_RUNTIME_DIR="${user_path}"
+      fi
 
-        if [ -z "$SYSTEMD_PID" ]; then
-          export XDG_RUNTIME_DIR="${user_path}"
-        fi
+      wslg_runtime_dir="/mnt/wslg/runtime-dir"
 
-        wslg_runtime_dir="/mnt/wslg/runtime-dir"
+      ln -fs "${wslg_runtime_dir}"/wayland-0 "${user_path}"/ 2>/dev/null
+      ln -fs "${wslg_runtime_dir}"/wayland-0.lock "${user_path}"/ 2>/dev/null
 
-        ln -fs "${wslg_runtime_dir}"/wayland-0 "${user_path}"/ 2>/dev/null
-        ln -fs "${wslg_runtime_dir}"/wayland-0.lock "${user_path}"/ 2>/dev/null
+      pulse_path="${user_path}/pulse"
+      wslg_pulse_dir="${wslg_runtime_dir}"/pulse
 
-        pulse_path="${user_path}/pulse"
-        wslg_pulse_dir="${wslg_runtime_dir}"/pulse
+      if [ ! -d "${pulse_path}" ]; then
+        mkdir -p "${pulse_path}" 2>/dev/null
 
-        if [ ! -d "${pulse_path}" ]; then
-          mkdir -p "${pulse_path}" 2>/dev/null
+        ln -fs "${wslg_pulse_dir}"/native "${pulse_path}"/ 2>/dev/null
+        ln -fs "${wslg_pulse_dir}"/pid "${pulse_path}"/ 2>/dev/null
+      elif [ -S "${pulse_path}/native" ]; then
+        rm -f "${pulse_path}/native" 2>/dev/null
+        ln -fs "${wslg_pulse_dir}"/native "${pulse_path}"/ 2>/dev/null
+      fi
 
-          ln -fs "${wslg_pulse_dir}"/native "${pulse_path}"/ 2>/dev/null
-          ln -fs "${wslg_pulse_dir}"/pid "${pulse_path}"/ 2>/dev/null
-
-        elif [ -S "${pulse_path}/native" ]; then
-          rm -f "${pulse_path}/native" 2>/dev/null
-          ln -s "${wslg_pulse_dir}"/native "${pulse_path}"/ 2>/dev/null
-        fi
-
-        unset user_path
-        unset wslg_runtime_dir
-        unset wslg_pulse_dir
-        unset pulse_path
-        unset uid
+      unset user_path
+      unset wslg_runtime_dir
+      unset wslg_pulse_dir
+      unset pulse_path
+      unset uid
 
       return
     fi
@@ -175,7 +206,7 @@ main() {
   alias wsl='wsl.exe'
 
   if [ -n "${WSL2}" ]; then
-    #Setup video acceleration
+    # Setup video acceleration
     export VDPAU_DRIVER=d3d12
     export LIBVA_DRIVER_NAME=d3d12
 
@@ -184,10 +215,9 @@ main() {
   fi
 
   if [ -z "$SYSTEMD_PID" ]; then
-
     save_environment
-
-  elif [ -n "$SYSTEMD_PID" ] && [ "$SYSTEMD_PID" -eq 1 ] && [ -f "$HOME/.systemd.env" ] && [ -n "$WSL_SYSTEMD_EXECUTION_ARGS" ]; then
+  elif [ -n "$SYSTEMD_PID" ] && [ "$SYSTEMD_PID" -eq 1 ] && [ -f "$HOME/.systemd.env" ] &&
+    [ -n "$WSL_SYSTEMD_EXECUTION_ARGS" ]; then
     # Only if built-in systemd was started
     set -a
     # shellcheck disable=SC1090
@@ -196,6 +226,8 @@ main() {
 
     setup_interop
   fi
+
+  define_xdg_environment
 
   # Check if we have Windows Path
   if [ -z "$WIN_HOME" ] && (command -v cmd.exe >/dev/null 2>&1); then
@@ -219,8 +251,8 @@ main() {
     fi
 
     unset win_home_lnk
-
   fi
+  unset systemd_saved_environment
 }
 
 main "$@"
